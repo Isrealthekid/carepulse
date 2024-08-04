022f49d1b4fde3b907672d47646d3ee9e564792f
isrealthekid <oluwoleisreal12@gmail.com>
2024-08-04 22:54:07 +0100

    second commit

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -12,6 +12,7 @@ import SubmitButton from "../ui/SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import 
 
 
 export enum FormFieldType {
@@ -45,11 +46,11 @@ const PatientForm = () => {
     setIsLoading(true);
 
     try {
-      // const userData = { name, email, phone };
+      const userData = { name, email, phone };
 
-      // const user = await createUser(userData);
+      const user = await createUser(userData);
 
-      // if(user) router.push(`/patients/${user.$id}/register`)
+      if(user) router.push(`/patients/${user.$id}/register`)
     } catch (error) {
       console.log(error)
     }
